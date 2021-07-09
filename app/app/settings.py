0450a341431a91fa4616687d7faa310590dec3a4
Florian <florian.gruenwald@hec.edu>
2021-07-09 12:24:13 +0200

    fix gh actions

--- app/app/settings.py
+++ app/app/settings.py
@@ -88,6 +88,18 @@ DATABASES = {
     }
 }
 
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '54321',
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
