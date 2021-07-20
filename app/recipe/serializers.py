17cebd56e389f00fb5db3d5eabbc04ed727c6d2e
Florian <florian.gruenwald@hec.edu>
2021-07-20 16:44:08 +0200

    add recipe image upload feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -4,42 +4,48 @@ from core.models import Tag, Ingredient, Recipe
 
 
 class TagSerializer(serializers.ModelSerializer):
-  """Serializer for tag objects"""
+    """Serializer for tag objects"""
 
-  class Meta:
-    model = Tag
-    fields = ('id', 'name')
-    read_only_fields = ('id',)
+    class Meta:
+        model = Tag
+        fields = ("id", "name")
+        read_only_fields = ("id",)
 
 
 class IngredientSerializer(serializers.ModelSerializer):
-  """Serializer for ingredient objects"""
+    """Serializer for ingredient objects"""
 
-  class Meta:
-    model = Ingredient
-    fields = ('id', 'name')
-    read_only_fields = ('id',)
+    class Meta:
+        model = Ingredient
+        fields = ("id", "name")
+        read_only_fields = ("id",)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-  """Serializes a recipe"""
-  ingredients = serializers.PrimaryKeyRelatedField(
-      many=True,
-      queryset=Ingredient.objects.all()
-  )
-  tags = serializers.PrimaryKeyRelatedField(
-      many=True,
-      queryset=Tag.objects.all()
-  )
+    """Serializes a recipe"""
 
-  class Meta:
-    model = Recipe
-    fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes',
-              'price', 'link')
-    read_only_fields = ('id',)
+    ingredients = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Ingredient.objects.all()
+    )
+    tags = serializers.PrimaryKeyRelatedField(many=True, queryset=Tag.objects.all())
+
+    class Meta:
+        model = Recipe
+        fields = ("id", "title", "ingredients", "tags", "time_minutes", "price", "link")
+        read_only_fields = ("id",)
 
 
 class RecipeDetailSerializer(RecipeSerializer):
-  """Serialize recipe detail"""
-  ingredients = IngredientSerializer(many=True, read_only=True)
-  tags = TagSerializer(many=True, read_only=True)
+    """Serialize recipe detail"""
+
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipes"""
+
+    class Meta:
+        model = Recipe
+        fields = ("id", "image")
+        read_only_fields = ("id",)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -7,52 +9,71 @@ from core.models import Tag, Ingredient, Recipe
 from recipe import serializers
 
 
-class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
-                            mixins.ListModelMixin,
-                            mixins.CreateModelMixin):
-  """Base viewset for user owned recipe attributes"""
-  authentication_classes = (TokenAuthentication,)
-  permission_classes = (IsAuthenticated,)
+class BaseRecipeAttrViewSet(
+    viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin
+):
+    """Base viewset for user owned recipe attributes"""
 
-  def get_queryset(self):
-    """Return objects for current authenticated user"""
-    return self.queryset.filter(user=self.request.user).order_by('-name')
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
-  def perform_create(self, serializer):
-    """Create new object for authenticated user"""
-    serializer.save(user=self.request.user)
+    def get_queryset(self):
+        """Return objects for current authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by("-name")
+
+    def perform_create(self, serializer):
+        """Create new object for authenticated user"""
+        serializer.save(user=self.request.user)
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
-  """Manage tags in db"""
-  queryset = Tag.objects.all()
-  serializer_class = serializers.TagSerializer
+    """Manage tags in db"""
+
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
 
 
 class IngredientViewSet(BaseRecipeAttrViewSet):
-  """Manage ingredients in db"""
-  queryset = Ingredient.objects.all()
-  serializer_class = serializers.IngredientSerializer
+    """Manage ingredients in db"""
+
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
-  """Manage Recipes in db"""
-  serializer_class = serializers.RecipeSerializer
-  queryset = Recipe.objects.all()
-  authentication_classes = (TokenAuthentication,)
-  permission_classes = (IsAuthenticated,)
+    """Manage Recipes in db"""
 
-  def get_queryset(self):
-    """Retrieve the recipes for authenticated user"""
-    return self.queryset.filter(user=self.request.user).order_by('-id')
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
-  def get_serializer_class(self):
-    """Return appropriate serializer class"""
-    if self.action == 'retrieve':
-      return serializers.RecipeDetailSerializer
+    def get_queryset(self):
+        """Retrieve the recipes for authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by("-id")
 
-    return self.serializer_class
+    def get_serializer_class(self):
+        """Return appropriate serializer class"""
+        if self.action == "retrieve":
+            return serializers.RecipeDetailSerializer
 
-  def perform_create(self, serializer):
-    """Create new recipe"""
-    serializer.save(user=self.request.user)
+        elif self.action == "upload_image":
+            return serializers.RecipeImageSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create new recipe"""
+        serializer.save(user=self.request.user)
+
+    @action(methods=["POST"], detail=True, url_path="upload-image")
+    def upload_image(self, request, pk=None):
+        """Upload an image to a recipe"""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
